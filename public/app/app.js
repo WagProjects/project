1049c12f07a2299de9b5950197058101a926b233
Wagner Oliveira <contato-wagneros@outlook.com>
2024-01-30 01:12:13 -0300

    Complete

--- public/app/app.js
+++ public/app/app.js
@@ -1,9 +1,21 @@
-import { log } from "./utils/promise-helpers.js";
-import './utils/array-helpers.js'
-import { notasService as service} from "./nota/service.js";
+// app/app.js
+import { log, timeoutPromise, retry } from './utils/promise-helpers.js';
+import './utils/array-helpers.js';
+import { notasService as service } from './nota/service.js';
+import { takeUntil, debounceTime, partialize, pipe } from './utils/operators.js';
+import { EventEmitter } from './utils/event-emitter.js';
 
-document.querySelector('#myButton').onclick = () =>
-    service
-    .sumItems('2143')
-    .then(console.log)
+const operations = pipe(
+    partialize(takeUntil, 3),
+    partialize(debounceTime, 500)
+);
+
+const action = operations(() => 
+    retry(3, 3000, () => timeoutPromise(200, service.sumItems('2143')))
+    .then(total => EventEmitter.emit('itensTotalizados', total))
     .catch(console.log)
+);
+
+document
+.querySelector('#myButton')
+.onclick = action;
